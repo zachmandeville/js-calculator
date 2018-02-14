015a99db90467669aa089b3b716811e6a86cd603
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-02-14 13:50:29 -0500

    Created distinct functions and improved styling
    With this commit I broke apart that single function into fully separate,
    distinct purpose functions. I also separated the calculationb etween
    grabbing the right numbers and then doing the math function on them.
    
    Lastly, I improved the colors in the css

--- calculator.js
+++ calculator.js
@@ -1,15 +1,17 @@
-//Define some Global Variables
-
+/* Define some Global Variables
+-----------------------------*/
 var entries = []; //Collects each number string and arithmetic symbol for the final math
-var total = 0; //The final math.
 var temp = '' //Collects the button inputs until it becomes an official entry.
-var arithmetic = {
-  plus: '+',
-  minus: '-',
-  multiply: '*',
-  divide: '/'
-}
 
+
+/* Our Functions!
+---------------*/
+
+//set what is displayed on the calculator screen
+var display = function (val ) {
+  var screen = document.getElementById('screen')
+  screen.innerHTML = val;
+}
 //adds the number of the button pressed to our display and temp variable
 var enterNumber = function (val) {
     temp += val
@@ -29,42 +31,37 @@ var clearEntry = function () {
     display('0')
 }
 
-var testDisplay = function (val) {
-  var screen = document.getElementById('screen') //our calculator screen
-  if (val === 'cool') {
-    entries.push(temp)
-    calculate()
-  }
-
-  //if val is 'CE', or clear last entry, remove the temp file and leave things be.
-  else if (val === 'CE'){
-    temp = ''
-    display('whatup')
-  }
-  else if (arithmetic[val] !== -1) {
-    entries.push(temp)
-    entries.push(arithmetic[val])
-    temp = ''
-  }
-}
-
-var display = function (val ) {
-  var screen = document.getElementById('screen')
-  screen.innerHTML = val;
-}
-  
-var calculate = function () {
-  var nt = Number(entries[0])
-  for (var i = 1; i < entries.length; i++){
-    var nextNum = Number(entries[i+1])
-    var symbol = entries[i]
-    if (symbol === '+'){nt += nextNum}
-    else if (symbol === '-'){nt -= nextNum}
-    else if (symbol === '*'){nt *= nextNum}
-    else if (symbol === '/'){nt /= nextNum}
-    i++
-  }
-  display(nt)
-  entries = []
+//When a math symbol is pressed, add the current temp to the entries and then the math symbol.
+//Clear out temp so we can put in the next number.
+var addSymbol = function (val) {
+  entries.push(temp)
+  entries.push(val)
   temp = ''
 }
+
+//When equals is pressed, send the last temp to entries and start the calculations
+var pressEquals = function () {
+  entries.push(temp)
+  calculate()
+}
+
+
+//Grab all the entries and do math with them, then display the result and clear the caches.
+var calculate = function () {
+  var total = Number(entries[0])
+  for (var i = 1; i < entries.length; i+= 2){
+    total = doMath(entries[i], Number(entries[i+1]), total)
+  }
+  display(total)
+  entries = []
+  temp = ''
+  }
+
+//Parse the symbol entered and use that to do math with the current total and the given number.
+var doMath = function (symbol, number, total) {
+    if (symbol === '+'){total += number}
+    else if (symbol === '-'){total -= number}
+    else if (symbol === '*'){total *= number}
+    else if (symbol === '/'){total /= number}
+    return total
+  }
